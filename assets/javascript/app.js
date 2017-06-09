70232c60c828931dfa9f245b2595816f7b43de3a
david-mansen <davidmansen@gmail.com>
2017-06-09 10:30:41 -0400

    basic price request

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,47 @@
 $(document).ready(function(){
 
+  //build house objects from the onboard informatics api call,
+  //make calls to zillow to get the price/rent zestimate
+  function buildHouseObjects(housesJSON){
+    var houses = [];
 
-  
-  function buildPropertyObjects(propertiesJSON){
-    var properties = [];
+    console.log(housesJSON.property[0]);
 
-    console.log(propertiesJSON.property[0]);
-
-    for(let i=0; i<propertiesJSON.property.length; i++){
-      var temp = propertiesJSON.property[i];
-      var property = {
+    for(let i=0; i<housesJSON.property.length; i++){
+      var temp = housesJSON.property[i];
+      var house = {
         distance: temp.location.distance,
         latitude: temp.location.latitude,
-        longitude: temp.location.longitude
+        longitude: temp.location.longitude,
+        addr1: temp.address.line1,
+        addr2: temp.address.line2,
+        price: -1
       };
-      properties.push(property);
+      houses.push(house);
+      //getZestimate(house);
     }
-    console.log(properties);
-    return properties;
+    console.log(houses);
+    return houses;
   }
 
-  function retrieveProperties(){
+  //called to set up ajax request and  set callback
+  function retrieveHouses(){
     var url = "https://search.onboard-apis.com/propertyapi/v1.0.0/property/address";
     var parameters = $.param({
       'address1': "10838 Heather Ridge Circle",
       'address2': "Orlando, FL 32817",
       'radius': 10,
       'orderby': "distance",
-      'pagesize': 100
+      'pagesize': 10
     });
 
     url+="?"+parameters;
 
     if(window.localStorage.getItem(url)){
       console.log("retrieving from cache");
-      var cachedProperty = JSON.parse(window.localStorage.getItem(url));
-      //console.log(cachedProperty);
-      buildPropertyObjects(cachedProperty);
+      var cachedHouse = JSON.parse(window.localStorage.getItem(url));
+      //console.log(cachedHouse);
+      buildHouseObjects(cachedHouse);
     }
     else{
       console.log("Making request to",url);
@@ -57,36 +62,41 @@ $(document).ready(function(){
       $.ajax(settings).done(function (response) {
         //console.log(response);
         window.localStorage.setItem(url, JSON.stringify(response));
-        buildPropertyObjects(response);
+        buildHouseObjects(response);
       });
     }
   }
 
-  // function zillowTest(){
-  //   var url = "http://www.zillow.com/webservice/GetSearchResults.htm"
-  //
-  //   var parameters = $.param({
-  //     'zws-id': "X1-ZWz1962gltdszv_4srxq",
-  //     'address': "6773 Red Reef Street",
-  //     'citystatezip': "Lake Worth, FL",
-  //     'rentzestimate': "false"
-  //   });
-  //
-  //   url+= "?"+parameters;
-  //
-  //   $(document).ready(function () {
-  //     $.ajax({
-  //       type: "GET",
-  //       url: url,
-  //       dataType: "xml",
-  //       success: function(xml) {
-  //         console.log(xml);
-  //
-  //       }
-  //     });
-  //   });
-  // }
+  //request zestimate from zillow for each house
+  function getZestimate(house){
+    if(house.price != -1){
+      console.log("already got the price");
+    }
+    else{
+      var url = "http://www.zillow.com/webservice/GetSearchResults.htm"
+
+      var parameters = $.param({
+        'zws-id': "X1-ZWz1962gltdszv_4srxq",
+        'address': house.addr1,
+        'citystatezip': house.addr2,
+        'rentzestimate': "true"
+      });
+
+      url+= "?"+parameters;
+
+      $.ajax({
+        type: "GET",
+        url: url,
+        dataType: "xml",
+        success: function(xml) {
+          console.log(xml);
+          console.log("price request: ",xml.getElementsByTagName("amount").text());
+        }
+      });
+    }
+
+  }
 
 
-  $("#startButtonInner").on("click", retrieveProperties);
+  $("#startButtonInner").on("click", retrieveHouses);
 });
